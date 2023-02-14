90f6f747dd3275e6542a5070b2afcaa5854e68d4
Meng Zhang <97078589+MengZ9@users.noreply.github.com>
2023-02-13 23:01:24 -0800

    finished lab 6.16

--- §6.16LAB_ JavaScript password strength/script.js
+++ §6.16LAB_ JavaScript password strength/script.js
@@ -1,1 +1,13 @@
 // Your solution goes here 
+function isStrongPassword(password){
+    if(password.length < 8){
+        return false;
+    }
+    
+    if(password.includes("password")){
+        return false;
+    }
+
+    return  /[A-Z]/.test(password);
+
+}
