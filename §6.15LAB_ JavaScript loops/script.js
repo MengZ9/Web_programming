39590cd29512f200089112864061857506972cdc
Meng Zhang <97078589+MengZ9@users.noreply.github.com>
2023-02-13 17:31:52 -0800

    finished

--- §6.15LAB_ JavaScript loops/script.js
+++ §6.15LAB_ JavaScript loops/script.js
@@ -1,5 +1,11 @@
 function drawTriangle(size) {
-
+   for(let i = 1; i <= size; i++){
+      let star = "";
+      for(let j = 0; j < i; j++){
+         star += "*";
+      }
+      console.log(star);
+   }
    // Your solution goes here
    
 }
