f5518423a68f187dfd764567a4dfb311ef715cbd
Meng Zhang <97078589+MengZ9@users.noreply.github.com>
2023-02-19 01:19:25 -0800

    completed 6.17

--- §6.17LAB_ JavaScript number guessing game/script.js
+++ §6.17LAB_ JavaScript number guessing game/script.js
@@ -1,1 +1,29 @@
-// Your solution goes here 
+function playGuessingGame(numToGuess, totalGuesses = 10){
+    let count = 0;
+    let answer;
+    while(count < totalGuesses){
+        answer = prompt(count === 0 ? "Enter a number between 1 and 100." :
+        (isNaN(answer) ? "Please enter a number." :
+        (answer < numToGuess ? `${answer} is too small. Guess a larger number.` :
+        `${answer} is too large. Guess a smaller number.`)));
+
+        if(answer === null){
+            break;
+        }
+
+        answer = Number(answer);
+
+        if (isNaN(answer)){
+            continue;
+        }
+
+        count++;
+
+        if (answer === numToGuess){
+            return count;
+        }
+    }
+    return 0;
+}// Your solution goes here 
+
+
