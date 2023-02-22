23944c74f79777e1930e1924f67085369d19f5b5
Meng Zhang <97078589+MengZ9@users.noreply.github.com>
2023-02-22 01:04:21 -0800

    completed §6.20

--- §6.20LAB_ JavaScript maps/script.js
+++ §6.20LAB_ JavaScript maps/script.js
@@ -1,3 +1,31 @@
 function calcWordFrequencies() {
-   
+    let read = prompt("read a list of words");
+    let list = read.trim().split(" ");
+
+    let result = new Map();
+    
+    if(list.length == null){
+        return 0;
+    }
+    
+
+    for(let i = 0; i < list.length; i++){
+        let temp = list[i];
+
+        if(result.has(temp)){
+            let count = result.get(temp);
+            result.set(temp, count + 1);
+        }
+        else{
+            result.set(temp, 1);
+        }
+    }
+
+    function print(key, val){
+        console.log(val + " " + key);
+    }
+
+    result.forEach(print);
+
+
 }
