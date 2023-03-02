2972066db72f1f240e4d024709a95a20113ff2fe
Meng Zhang <97078589+MengZ9@users.noreply.github.com>
2023-03-02 00:31:55 -0800

    §7.18

--- §7.18 LAB_ Quote web API (XMLHttpRequest)/quote.js
+++ §7.18 LAB_ Quote web API (XMLHttpRequest)/quote.js
@@ -14,14 +14,38 @@ window.addEventListener("DOMContentLoaded", function () {
 
 function fetchQuotes(topic, count) {
    // TODO: Modify to use XMLHttpRequest
+   let xhr = new XMLHttpRequest();
+   let url = `https://wp.zybooks.com/quotes.php?topic=${topic}&count=${count}`;
+   xhr.responseType = "json";
+   xhr.open("GET", url);
+   xhr.addEventListener("load", responseReceivedHandler); 
+   xhr.send();
 
+
+         //default
    let html = "<ol>";
    for (let c = 1; c <= count; c++) {
       html += `<li>Quote ${c} - Anonymous</li>`;
    }
    html += "</ol>";
-
    document.querySelector("#quotes").innerHTML = html;
 }
 
 // TODO: Add responseReceivedHandler() here
+
+function responseReceivedHandler(){
+   const quote = this.response;
+   document.querySelector("#quotes").innerHTML = "";
+   if(this.status === 200){
+      let html = "<ol>";
+      for (let j = 0; j < quote.length; j++) {
+         html += `<li>${quote[j].quote} - ${quote[j].source}</li>`;
+      }
+      html += "</ol>";
+     document.querySelector("#quotes").innerHTML = html;
+   }
+   else{
+      document.querySelector("#quotes").innerHTML = "";
+      document.querySelector("#quotes").innerHTML = `Topic '${this.response.error}' not found`; 
+   }
+}
